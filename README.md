a24dda9be118d2c174c56c1c9e5dff1312300d67
Swyzsh <swyzsh@protonmail.com>
2024-08-25 01:04:10 +0530

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple web based stopwatch [React + Vite]
---
+++ src/Stopwatch.jsx
@@ -0,0 +1,11 @@
+import './Stopwatch.css';
+
+function Stopwatch() {
+  return (
+    <div className="stopwatch">
+
+    </div>
+  );
+}
+
+export default Stopwatch;
